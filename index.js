296926670c930f676069799a2564f5c2f978a069
Ammon Jeffery <savingphace@gmail.com>
2020-07-22 17:10:38 -0600

    Installed util and fs

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
 // array of questions for user
 const questions = [
-
+    {
+        type:"input",
+        name:"Title",
+        message: "What is the Title of your README?"
+    },
+    {
+        type:"input",
+        name: "Description",
+        message: "Enter a Description of your README"
+    },
+    {
+        type:"list",
+        name:"Installation Instructions",
+        message: "Enter Installation Instructions"
+    }
 ];
+const util = require("util");const writeFileAsync = util.promisify(fs.writeFile);function promptUser() {
+return inquirer.prompt(questions)
+
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
