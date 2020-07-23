0dd9154c15d85a7f5f620e7323ef3ac882e4bafa
Ammon Jeffery <savingphace@gmail.com>
2020-07-22 18:09:23 -0600

    finished questions, beginning testing with Node

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util")
+const util = require("util");
 
-const 
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// array of questions for user
-const questions = [
+function promptUser() {
+    return inquirer.prompt ([
+
     {
         type:"input",
         name:"Title",
-        message: "What is the Title of your README?"
+        message: "What is the Title of your README?",
+        validate: catchEmpty
     },
     {
         type:"input",
         name: "Description",
-        message: "Enter a Description of your README"
+        message: "Enter a Description of your README",
+        validate: catchEmpty
     },
     {
-        type:"list",
+        type:"input",
         name:"Installation Instructions",
-        message: "Enter Installation Instructions"
+        message: "Enter Installation Instructions",
+        validate: catchEmpty
+    },
+    {
+        type : "input",
+        name: "Usage",
+        message: "Enter Usage Instructions",
+        validate: catchEmpty
+    },
+    {
+        type: "list",
+        name: "License",
+        message: "Please select which licenses you wish to use",
+        choices: ["Apache 2.0", "MIT", "BSD 3","None"],
+        validate: catchEmpty
     }
-];
+    ]);
+}
+
+function catchEmpty (value){
+    if (value === ""){
+        return "Please enter requested information"
+    }
+    else return true;
+} 
